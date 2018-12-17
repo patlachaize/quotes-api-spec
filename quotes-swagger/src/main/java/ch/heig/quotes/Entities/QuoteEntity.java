ca7349eba9eb7af2a1028529dbc7767b7ef9f13a
Patrick Lachaize <patrick.lachaize@heig-vd.ch>
2018-12-17 17:47:44 +0100

    suppress id in Quote payload when addQuoteUsingPost

--- quotes-swagger/src/main/java/ch/heig/quotes/Entities/QuoteEntity.java
+++ quotes-swagger/src/main/java/ch/heig/quotes/Entities/QuoteEntity.java
@@ -18,20 +18,27 @@ public class QuoteEntity {
 
     public QuoteEntity() {}
 
-    public QuoteEntity(int id, String author, String citation) {
-        this.id = id;
-        this.author = author;
-        this.citation = citation;
+    public int getId() {
+        return id;
     }
 
-    public int getId() { return id; }
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getAuthor() {
         return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public String getCitation() {
         return citation;
     }
 
+    public void setCitation(String citation) {
+        this.citation = citation;
+    }
 }
